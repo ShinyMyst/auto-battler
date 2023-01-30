8daaf1256f194069799bcd68b22b38416147e4ef
Glen Godby <122401790+ShinyMyst@users.noreply.github.com>
2023-01-29 23:07:01 -0500

    Added ASCII from web
    Need a more solid system for how things are divided.  Scenes will not always be static.  Class should also serve a purpose that calling data directly cannot do.

--- main/main.py
+++ main/main.py
@@ -1,9 +1,6 @@
-
-
-
 class Game():
     def __init__(self):
-        pass
+        self.data = Data()
 
 
     def run(self):
@@ -37,13 +34,42 @@ class Scene():
 class Data():
     def __init__(self):
         self.scenes = {
-            start_screen: None,
-            symbol_select: None,
-            character_select: None,
-            preparation_phase: None
+            "start_screen": Scene(start_text),
+            "symbol_select": None,
+            "character_select": None,
+            "preparation_phase": None
             }
 
 """ I am not sure if I want every single scene to be listed here or
 to create sub-scenes withint he scene class.  Sub-scenes is more organized
 BUT the entire purpose of the scene class is to have each individual
 screen as an object.  Making sub-classes muddles the purpose."""
+
+
+ascii_skull = [
+"           ______ ",
+"        .-'      '-.",
+"       /            \\",
+"      |              |",
+"      |,  .-.  .-.  ,|",
+"      | )(__/  \__)( |",
+"      |/     /\     \|",
+"      (_     ^^     _)",
+"       \__|IIIIII|__/",
+"        | \IIIIII/ |",
+"        \          /",
+"         `--------`"
+]
+
+ascii_exhume = [
+" ______      _                          ",
+" |  ____|    | |                         ",
+" | |__  __  _| |__  _   _ _ __ ___   ___ ",
+" |  __| \ \/ / '_ \| | | | '_ ` _ \ / _ \\",
+" | |____ >  <| | | | |_| | | | | | |  __/",
+" |______/_/\_\_| |_|\__,_|_| |_| |_|\___|"                                                 
+]
+
+start_text = ascii_exhume + ascii_skull + ["Rise of the Necromancer"]
+
+
